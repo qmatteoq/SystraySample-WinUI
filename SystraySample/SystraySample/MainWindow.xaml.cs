f2285779324ab36c0658f41cd9c870f34ea69874
Matteo Pagani <matteo.pagani@microsoft.com>
2021-03-30 17:20:14 +0200

    Add project files.

---
+++ SystraySample/SystraySample/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SystraySample
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+
+            var tb = Application.Current.Resources["MyNotifyIcon"] as TaskbarIcon;
+            string result = Assembly.GetExecutingAssembly().Location;
+            int index = result.LastIndexOf("\\");
+            string iconPath = $"{result.Substring(0, index)}\\Icons\\error.ico";
+            tb.Icon = new System.Drawing.Icon(iconPath);
+            tb.ContextMenu = new System.Windows.Controls.ContextMenu();
+
+            tb.ContextMenu.Items.Add("Option 1");
+            tb.ContextMenu.Items.Add("Option 2");
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
